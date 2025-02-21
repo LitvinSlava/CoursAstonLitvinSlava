a8313f6048455949c3cb60b7cf66d37c24e61be1
Slava Litvin <litvinslava86@gmail.com>
2025-02-21 17:29:08 +0300

    class сотрудник, процесс

---
+++ src/main/lecture_4/Employee.java
@@ -0,0 +1,28 @@
+package src.main.lecture_4;
+
+/** Задание №1 Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+ Конструктор класса должен заполнять эти поля при создании объекта.
+ Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.**/
+
+public class Employee {
+    private String fullName;
+    private String jobTitle;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String jobTitle, String email, String phone, int salary, int age){
+        this.fullName = fullName;
+        this.jobTitle = jobTitle;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void printEmployeeInfo(){
+
+    }
+
+
+}
