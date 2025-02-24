77d68dcfe7d48f0bd58ab0fcd4cb6821e09cfdb4
Slava Litvin <litvinslava86@gmail.com>
2025-02-24 14:10:18 +0300

    задание 3, для четвертой лекции

--- src/main/lecture_4/Exercise4.java
+++ src/main/lecture_4/Exercise4.java
@@ -19,7 +19,7 @@ public class Exercise4 {
          ...
          persArray[4] = new Person(...);}
          }**/
-        Employee[] employees = new Employee[5];
+/*      Employee[] employees = new Employee[5];
         employees[0] = new Employee("Литвин Вячеслав Васильевич", "инженер по тестированию", "litvinslava1986@gmail.com", "+375292115600", 10000000, 38);
         employees[1] = new Employee("Литвин Арсений Вячеславович", "эксперт по прокрастенации'", "litvinArseniy2014@gmail.com", "+375292115601", 100000000, 11);
         employees[2] = new Employee("Литвин Виктория Владимировна", "инженер по домохозяйству", "litvinVika1989@gmail.com", "+375292115602", 1000000, 36);
@@ -29,9 +29,23 @@ public class Exercise4 {
         for (Employee employee: employees) {
             employee.printEmployeeInfo();
             System.out.println();
-        }
+        }*/
         /** Задание №3 Создать класс Park с внутренним классом, с помощью объектов
          которого можно хранить информацию об аттракционах, времени их
          работы и стоимости.**/
+        Park park = new Park("Центральный парк", "Полоцк", 862, 341.0, 10000000);
+        park.printParkInfo();
+        System.out.println();
+
+        Park.Attraction attraction1 = park.new Attraction("Колесо оборзения", "10:00 - 22:00", 5.0);
+        Park.Attraction attraction2 = park.new Attraction("Американские горки", "11:00 - 20:00", 7.0);
+        Park.Attraction attraction3 = park.new Attraction("Карусель", "09:00 - 18:00", 3.0);
+
+        System.out.println("Аттракционы парка:");
+        attraction1.printInfo();
+        System.out.println();
+        attraction2.printInfo();
+        System.out.println();
+        attraction3.printInfo();
     }
 }
---
+++ src/main/lecture_4/Park.java
@@ -0,0 +1,45 @@
+package src.main.lecture_4;
+
+/** Задание №3 Создать класс Park с внутренним классом, с помощью объектов
+ которого можно хранить информацию об аттракционах, времени их
+ работы и стоимости.**/
+
+public class Park {
+    private String name;
+    private String location;
+    private int yearStart;
+    private double area;
+    private int visitorsCount;
+
+    public Park(String name, String location, int yearStart, double area, int visitorsCount) {
+        this.name = name;
+        this.location = location;
+        this.yearStart = yearStart;
+        this.area = area;
+        this.visitorsCount = visitorsCount;
+    }
+
+    public class Attraction {
+        private String name;
+        private double price;
+        private String workTime;
+
+        public Attraction(String name, String workTime, double price) {
+            this.name = name;
+            this.workTime = workTime;
+            this.price = price;
+        }
+        public void printInfo() {
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Стоимость: " + price + " руб.");
+            System.out.println("Время работы: " + workTime);
+        }
+    }
+    public void printParkInfo() {
+        System.out.println("Парк: " + name);
+        System.out.println("Местоположение: " + location);
+        System.out.println("Год основания: " + yearStart);
+        System.out.println("Площадь: " + area + " га");
+        System.out.println("Посетителей в год: " + visitorsCount);
+    }
+}
