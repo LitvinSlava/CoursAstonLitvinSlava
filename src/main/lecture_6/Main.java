b20fdc9e39df07451e5fd19b5b417dffc14e7de8
Slava Litvin <litvinslava86@gmail.com>
2025-03-19 16:41:50 +0300

    Добавли еще клас и исправил задание на 2 в коммите

--- src/main/lecture_6/Main.java
+++ src/main/lecture_6/Main.java
@@ -3,6 +3,6 @@ package src.main.lecture_6;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Лекция 6 задание 1");
+        System.out.println("Лекция 6 задание 2");
     }
 }
