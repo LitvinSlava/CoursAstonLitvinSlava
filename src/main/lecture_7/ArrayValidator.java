13e227d38ea7ee83a258ebd66608de8073954006
Slava Litvin <litvinslava86@gmail.com>
2025-05-06 16:41:38 +0300

    проверка длинны строки

--- src/main/lecture_7/ArrayValidator.java
+++ src/main/lecture_7/ArrayValidator.java
@@ -7,4 +7,11 @@ public class ArrayValidator {
             throw new MyArraySizeException("Неверный размер массива. Ожидается 4x4, получено " + array.length + "x?");
         }
     //Проверка длинны строки
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException(
+                        "Неверный размер строки " + i + ". Ожидается 4, получено " + array[i].length);
+            }
+        }
+    }
 }
