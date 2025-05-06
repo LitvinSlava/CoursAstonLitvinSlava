7bbee8648a7960f0eb33c45da4e5e4e0f899af51
Slava Litvin <litvinslava86@gmail.com>
2025-05-06 16:17:33 +0300

    правки

--- src/main/lecture_7/ArrayValidator.java
+++ src/main/lecture_7/ArrayValidator.java
@@ -2,5 +2,9 @@ package src.main.lecture_7;
 
 public class ArrayValidator {
     //Проверка размера массива
+    public static void checkArraySize(String[][] array) throws MyArraySizeException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Неверный размер массива. Ожидается 4x4, получено " + array.length + "x?");
+        }
     //Проверка длинны строки
 }
--- src/main/lecture_7/Main.java
+++ src/main/lecture_7/Main.java
@@ -1,4 +1,7 @@
 package src.main.lecture_7;
 
 public class Main {
+    public static void main(String[] args) {
+        git
+    }
 }
