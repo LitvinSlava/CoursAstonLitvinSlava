459703bbb6e3fd2d6f0ba979147f4466dc0b053b
Slava Litvin <litvinslava86@gmail.com>
2025-05-06 17:41:12 +0300

    1 часть

--- src/main/lecture_7/Main.java
+++ src/main/lecture_7/Main.java
@@ -1,7 +1,20 @@
 package src.main.lecture_7;
+import static src.main.lecture_7.ArrayValidator.checkArraySize;
 
 public class Main {
     public static void main(String[] args) {
-        git
+        // Пример корректного массива
+        String[][] correctArray = new String[4][4];
+
+        // Пример массива с ошибкой
+        String[][] invalidArray = new String[4][5];
+
+        try {
+            checkArraySize(correctArray); // Пройдет без ошибок
+            checkArraySize(invalidArray);  // Выбросит исключение
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        }
     }
 }
+
